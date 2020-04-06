cf52157df00cb4de3b1fcbd14c8ec91160f631e8
mission-deny-the-mission <mission.deny.the.mission@gmail.com>
2020-04-06 20:52:52 +0100

    add more comments

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include "classes.h"
 
 int main() {
+    // since demoClass is inside a different namespace :: has to be used
     classes::demoClass one("text", 14);
+
+    // simlar to how parts of the standard library have to be used with std::
+    // like blow
     std::cout << one << std::endl;
 }
